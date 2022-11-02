0f54bfa436b4f3576275376e68afcaaefbc53ecf
ilias <ilias.fasilakhs@gmail.com>
2022-11-02 16:07:47 +0200

    adding Lap

--- app2.js
+++ app2.js
@@ -6,8 +6,10 @@ let timeDisplay = document.getElementById("time__display");
 let startButton = document.getElementById("start");
 let pauseButton = document.getElementById("pause");
 let resetButton = document.getElementById("reset");
-let intervalId ;
-
+let lapButton = document.getElementById("lap");
+let lapList = document.querySelector(".lap__list");
+let intervalId;
+let n=0
 
 //create a function to update our time wich will run in the setInterval
 function updateTime() {
@@ -28,12 +30,18 @@ function updateTime() {
   let m = minutes < 10 ? "0" + minutes : minutes;
   let s = seconds < 10 ? "0" + seconds : seconds;
   let ms =
-  milliseconds < 10
-    ? "00" + milliseconds
-    : milliseconds < 100
-    ? "0" + milliseconds
-    : milliseconds;
+    milliseconds < 10
+      ? "00" + milliseconds
+      : milliseconds < 100
+      ? "0" + milliseconds
+      : milliseconds;
   timeDisplay.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+}
+
+function removeChild() {
+  while (lapList.hasChildElements()) {
+    lapList.removeChild(lapList.lastChild);
+  }
 }
 
 //addEvent listeners to our buttons
@@ -41,20 +49,59 @@ function updateTime() {
 document.getElementById("start").addEventListener("click", () => {
   if (intervalId !== null) {
     clearInterval(intervalId);
-    
   }
-  intervalId = setInterval(updateTime,10); 
+  intervalId = setInterval(updateTime, 10);
   // setInterval takes as parameters a function that will run continiously every 10ms as the second parameter
 });
+
+
+
 
 document.getElementById("pause").addEventListener("click", () => {
   clearInterval(intervalId);
 });
+
+
+
 document.getElementById("reset").addEventListener("click", () => {
+    while (lapList.firstChild) {
+        lapList.removeChild(lapList.lastChild);
+      }
   clearInterval(intervalId);
   [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
   timeDisplay.innerHTML = "00 : 00 : 00 : 000";
+  n=0;
+  
 });
 
-//Adding zeros and finilazing the display
+document.getElementById("lap").addEventListener("click", () => {
+  
+  n++
+  const lapItem = document.createElement("div");
+  lapItem.classList.add('lap__item')
+  
 
+
+  const paraTime = document.createElement('p')
+  paraTime.classList.add('para__time')
+  paraTime.innerHTML =  `${n}. ` + timeDisplay.innerHTML 
+
+  const lapButton = document.createElement("button")
+  lapButton.classList.add("lap__button");
+  lapButton.innerHTML = "DELETE";
+  
+
+  
+  lapList.appendChild(lapItem);
+  lapItem.appendChild(paraTime);
+  lapItem.appendChild(lapButton);
+  
+});
+
+
+document.querySelectorAll('.lap__button').addEventListener('click', ()=>{
+
+
+})
+
+//Adding zeros and finilazing the display
