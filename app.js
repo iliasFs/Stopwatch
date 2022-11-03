b2dedebf5054f3a54acc22c05af519562906157c
ilias <ilias.fasilakhs@gmail.com>
2022-11-03 18:17:06 +0200

    Stopwatch completed + nightmdode

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const css = document.querySelector('#cssfile')
 const icon = document.querySelector('#icon')
-
+let change = true
 icon.addEventListener('click', ()=>{
 
 if(icon.hasAttributes('fa-sun')){
-  css.setAttribute('href', 'styles2.css')
-  icon.classList.remove('fa-sun')
-  icon.classList.add('fa-moon')
-}
-
-else if(icon.hasAttributes('fa-moon'))
-{
-  css.setAttribute('href', 'styles.css')
-  icon.classList.remove('fa-moon')
-  icon.classList.add('fa-sun')
-
+    if (change){
+        css.setAttribute('href', 'styles2.css')
+        icon.classList.remove('fa-sun')
+        icon.classList.add('fa-moon')
+        change = false
+    }
+    else{
+        css.setAttribute('href', 'styles.css')
+        icon.classList.remove('fa-moon')
+        icon.classList.add('fa-sun')
+        change = true
+    }
 }
 
 
--- app2.js
+++ app2.js
@@ -95,7 +95,7 @@ document.getElementById("lap").addEventListener("click", () => {
     element.addEventListener('click', ()=>{
     
         element.parentElement.remove()
-        n--
+        
     })
     
   })
@@ -104,6 +104,29 @@ document.getElementById("lap").addEventListener("click", () => {
 
 
 
+// DARK-MODE
+
+const css = document.querySelector('#cssfile')
+const icon = document.querySelector('#icon')
+let change = true
+icon.addEventListener('click', ()=>{
+
+if(icon.hasAttributes('fa-moon')){
+    if (change){
+        css.setAttribute('href', 'styles2.css')
+        icon.classList.remove('fa-moon')
+        icon.classList.add('fa-sun')
+        change = false
+    }
+    else{
+        css.setAttribute('href', 'styles.css')
+        icon.classList.remove('fa-sun')
+        icon.classList.add('fa-moon')
+        change = true
+    }
+}
 
 
-//Adding zeros and finilazing the display
+
+})
+
